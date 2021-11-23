f511a9f6c1e0eb9b10811edab8e9035268811c50
Linhe Sun <52153080+linhesun@users.noreply.github.com>
2021-11-23 09:36:09 +0800

    Update r21.r

--- 2021_r21_china_map/r21.r
+++ 2021_r21_china_map/r21.r
@@ -1,30 +1,31 @@
-# url for map_data download: https://datav.aliyun.com/tools/atlas/index.html
+
+# https://datav.aliyun.com/tools/atlas/index.html
 install.packages('rgdal')
 install.packages('mapproj')
 library(rgdal)
 library(ggplot2)
-china1 <- readOGR(dsn = "china.geojason", stringsAsFactors = FALSE)
-Encoding(china1@data$name) <- "UTF-8"
-china2 <- fortify(china1)
+map1 <- readOGR(dsn = "china.geojason", stringsAsFactors = FALSE)
+Encoding(map1@data$name) <- "UTF-8"
+map2 <- fortify(map1)
 
-ggplot(china2) + 
+ggplot(map2) + 
   geom_polygon(aes(long, lat, group = group, fill = id), colour = 'white',
                show.legend = F)
 
 
 predat <- data.frame(id = 0:34,
                      val = runif(35,0,1),
-                     pro = china1@data$name)
-ind <- match(china2$id, predat$id)
-china2$val <- predat$val[ind]
-china2$pro <- predat$pro[ind]
+                     pro = map1@data$name)
+ind <- match(map2$id, predat$id)
+map2$val <- predat$val[ind]
+map2$pro <- predat$pro[ind]
 
-ggplot(china2) + 
+ggplot(map2) + 
   geom_polygon(aes(long, lat, group = group, fill = pro), colour = NA) +
   coord_map(projection = "azequidistant") +
   theme(text = element_text(family='Kai'))
 
-ggplot(china2) + 
+ggplot(map2) + 
   geom_polygon(aes(long, lat, group = group, fill = val), colour = NA) +
   coord_map(projection = "azequidistant") +
   scale_fill_gradientn(colours = rainbow(10)) +
